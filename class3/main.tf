a24f0513f29eaa26230e364367288521add58ae0
kazakbaevelaman <kazakbaevelaman066@gmail.com>
2024-07-06 21:44:56 +0000

    added class3

---
+++ class3/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "bastion"
+  public_key = file("~/.ssh/id_rsa.pub")
+  tags = local.common_tags
+}
+
+# filtering ami id with data block 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "web" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  #availability_zone = "us-east-2c"
+  subnet_id = "subnet-09d1991e3bc51847a"
+  count = 3
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+  #user_data_replace_on_change = true
+  tags = local.common_tags
+
+}
+
+
+output "ec2" {
+  value = aws_instance.web[0].public_ip 
+  #sensitive = true
+}
+
+
+
+
+
+
